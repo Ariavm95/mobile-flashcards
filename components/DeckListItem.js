de96b8413e4b6e9d570c24d3600ab8f6d4a7e25e
Ryan Chu <tianyuan.chu@gmail.com>
2017-10-14 13:42:27 -0700

    refactoring DeckListItem

---
+++ components/DeckListItem.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Card, CardItem, Text, Right, Icon} from 'native-base';
+
+export default class DeckListItem extends Component {
+    render() {
+        const item = this.props.item;
+        return (
+            <TouchableOpacity
+                onPress={() => this.props.navigation.navigate('deckView', {item})}>
+                <Card style={{
+                    marginLeft:30,
+                    marginRight:50,
+                    width:300,
+                    height:120
+                }}>
+                    <CardItem style={{
+                        flex:1,
+                        flexDirection:"row",
+                        justifyContent:"space-between",
+                        alignItems:"center"}}>
+                        <View style={{flex:1}}>
+                            <Text style={{
+                                fontSize: 25,
+                                fontWeight: 'bold'
+                            }}>
+                                {item.title}
+                            </Text>
+                            <Text>
+                                {item.questions.length} cards
+                            </Text>
+                        </View>
+                        <Right style={{paddingRight:5}}>
+                            <Icon name="arrow-forward" />
+                        </Right>
+                    </CardItem>
+                </Card>
+            </TouchableOpacity>
+        );
+    }
+}
--- components/DeckListView.js
+++ components/DeckListView.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import { FlatList, TouchableOpacity, View } from 'react-native';
-import { Card, CardItem, Text, Right, Icon} from 'native-base';
+import { FlatList } from 'react-native';
 import { getDecks } from '../db';
+import DeckListItem from './DeckListItem';
 
 export default class DeckListView extends Component {
     constructor(props) {
@@ -19,33 +19,14 @@ export default class DeckListView extends Component {
     }
 
     render() {
+        const navigation = this.props.navigation;
         return (
             <FlatList
                 style={{ paddingTop: 50 }}
                 data={this.state.decks}
                 keyExtractor={(item, index) => index}
                 renderItem={({item}) => (
-                    <TouchableOpacity
-                        onPress={() => this.props.navigation.navigate('deckView', {item})}>
-                        <Card style={{
-                            marginLeft:50,
-                            marginRight:50,
-                            height:60
-                        }}>
-                            <CardItem style={{
-                                flex:1,
-                                flexDirection:"row",
-                                justifyContent:"space-between",
-                                alignItems:"center"}}>
-                                <Text>
-                                    {item.title}
-                                </Text>
-                                <Right style={{paddingRight:5}}>
-                                    <Icon name="arrow-forward" />
-                                </Right>
-                            </CardItem>
-                        </Card>
-                    </TouchableOpacity>
+                    <DeckListItem navigation={navigation} item={item}/>
                 )}
             />
         );
